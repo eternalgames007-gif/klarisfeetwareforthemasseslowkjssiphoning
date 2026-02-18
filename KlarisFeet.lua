074f624c6653ade638dcc4f42ff666248d2aa7e4
eternalgames007-gif <eternalgames007@gmail.com>
2026-02-18 00:01:06 -0500

    Add files via upload

--- KlarisFeet.lua
+++ KlarisFeet.lua
@@ -231,8 +231,10 @@ function Lycoris.queueTeleport()
 	end
 
 	queue([[
+        repeat task.wait() until game:IsLoaded() and game:GetService("Players").LocalPlayer and game:GetService("Players").LocalPlayer:FindFirstChild("PlayerGui") and game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("TopbarGui")
+        task.wait(4)
         if not shared.Lycoris then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/eternalgames007-gif/klarisfeetwareforthemasseslowkjssiphoning/refs/heads/main/Preprocessed_Bundled.lua"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/eternalgames007-gif/klarisfeetwareforthemasseslowkjssiphoning/refs/heads/main/KlarisFeet.lua"))()
         end
     ]])
 end
