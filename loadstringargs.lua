f389577ee6d592f83e55fcdc1011d35c944d446f
eternalgames007-gif <eternalgames007@gmail.com>
2026-02-17 19:03:39 -0500

    Update loadstringargs.lua

--- loadstringargs.lua
+++ loadstringargs.lua
@@ -1,6 +1,7 @@
 -- deepwken
 if game.PlaceId == 4111023553 or game.PlaceId == 137000980549032 or game.PlaceId == 86761619761103 or game.PlaceId == 5735553160 or game.PlaceId == 6032399813 or game.PlaceId == 6473861193 or game.PlaceId == 6832934465 or game.PlaceId == 6832944305 or game.PlaceId == 8668476218 or game.PlaceId == 8712356310 or game.PlaceId == 9528956055 or game.PlaceId == 9598091549 or game.PlaceId == 9854906953 or game.PlaceId == 11108929042 or game.PlaceId == 12522881268 or game.PlaceId == 12522889886 or game.PlaceId == 12522892314  or game.PlaceId == 12559711136 or game.PlaceId == 13789332988 or game.PlaceId == 13891478131 or game.PlaceId == 14299563111 or game.PlaceId == 14759951085 or game.PlaceId == 14910041900 or game.PlaceId == 15043797592 or game.PlaceId == 15607970249 or game.PlaceId == 18139512873 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/eternalgames007-gif/klarisfeetwareforthemasseslowkjssiphoning/refs/heads/main/Preprocessed_Bundled.lua"))()
+end
 -- TypeSoul
 --local suc2, scr2 = pcall(game.HttpGet, game, "insert typesoul klarisfeet")
 --if suc2 and scr2 then 
