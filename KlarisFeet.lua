5db4f6e05ec49cef9c570707bb64fc89f805645c
eternalgames007-gif <eternalgames007@gmail.com>
2026-02-22 02:33:40 -0500

    Add files via upload

--- KlarisFeet.lua
+++ KlarisFeet.lua
@@ -13889,6 +13889,56 @@ local INTERNAL_MODULES = {
         	return self:action(timing, action)
         end
     end,
+    ["GenericTelegraph"] = function()
+        ---@type Action
+        local Action = getfenv().Action
+
+        -- Services.
+        local players = game:GetService("Players")
+
+        ---Module function.
+        ---@param self EffectDefender
+        ---@param timing EffectTiming
+        return function(self, timing)
+            local selfCharacter = players.LocalPlayer.Character;
+        	if self.owner ~= selfCharacter then
+        		return
+        	end
+
+
+            local effectData = self.data;
+
+            local telegraphType = effectData.telegraph;
+            local part = effectData.part;
+
+            if part and part:IsDescendantOf(selfCharacter) and part.Name == "HumanoidRootPart" then --Weapon Manual Telegraph (caster: self.character & part: hrp)
+                local dur = effectData.dur; --note: if you are working on this module, the real game code has a or 0.5 here, but it's not needed for weapon manual.
+                if dur ~= 1 then
+                    return;
+                end
+
+                if not table.find({
+                    "dodge_only",
+                    "block_only",
+                    "parry_only"
+                }, telegraphType) then return; end
+
+        	    self:hook("target", function(_)
+        	    	return true
+        	    end)
+
+        	    local action = Action.new()
+        	    action._when = 950 -- it is 1s but we do want to parry earlier due to the fact the user might not have 'Ping Compensation' on.
+        	    action._type = telegraphType == "dodge_only" and "Dodge" or "Parry"
+        	    action.ihbc = true
+        	    action.name = "Dynamic Weapon Manual Timing"       
+
+                return self:action(timing, action)
+            end
+
+        	return;
+        end
+    end,
     ["GlacialArc"] = function()
         ---@type Action
         local Action = getfenv().Action
@@ -66099,7 +66149,6 @@ end
 
 -- Return AnimatorDefender module.
 return AnimatorDefender
-
 end)
 __bundle_register("Game/Timings/PlaybackData", function(require, _LOADED, __bundle_register, __bundle_modules)
 ---@class PlaybackData
